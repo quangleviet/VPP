5fc554ec8d5f81861cfa4252a9526e76e92136f0
quanglvm <quangleviet0611@gmail.com>
2021-11-13 10:29:38 +0700

    edit

--- stationery/views.py
+++ stationery/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 # Create your views here.
-def login(request):
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get("email", "")
+        password = request.POST.get ("password", "")
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            my_message = (
+            "Your email address or password is incorrect. Please login again!"
+            )
+            messages.error(request, my_message)
+            
+            return render(request, "layouts/login.html")
+    
+        login(request, user)
+        return HttpResponseRedirect("dashboard/")
+
     return render(request, "layouts/login.html")
 def dashboard(request):
     return render(request, "dashboard/index.html")
